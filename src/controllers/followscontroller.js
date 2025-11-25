d4049d0467cd4c6c6f3cb5acc3030c47a2084043
Denilson valero <denilsonvaleromail.com>
2025-11-25 16:47:18 -0300

    feat/feed_follwed :sparkles:

--- src/controllers/followscontroller.js
+++ src/controllers/followscontroller.js
@@ -54,4 +54,20 @@ export const unfollowUser = async (req, res)=>{
 
 };
 
-
+export const feedfollowers = async (req,res)=>{
+    try {
+        const userId = req.user.user_id;
+        const [feed] = await db.query(
+            `SELECT p.post_id, p.content, p.image_url, p.created_at, u.user_id, u.user_name FROM posts p
+            JOIN users u ON p.user_id = u.user_id
+            JOIN follows f ON u.user_id = f.followed_id WHERE f.follower_id = ? ORDER BY p.created_at DESC`,
+            [userId]
+        );
+        if (feed.length === 0) {
+            return res.status(404).json({msg:"este usuario no tiene posts"})
+        }
+        res.status(200).json({msg:"feed de los usuarios que sigues",data:feed});
+    } catch (error) {
+        res.status(500).json({msg:"error al obtener feed"})
+    }
+}

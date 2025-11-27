295c26c089ffad503fb345787528a4ff48fef50b
Denilson valero <denilsonvaleromail.com>
2025-11-27 13:48:21 -0300

    feat/updateprofile :construction:

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,3 +65,19 @@ export const login= async (req,res)=>{
     }
 }
 
+export const updateProfile = async (req,res) =>{
+    try{
+        const userId = req.user.user_id;
+        const {user_name,bio,location}= req.body;
+
+        const [existinguser]= await db.query ("SELECT * FROM users WHERE user_id = ?",[userId]);
+        if(existinguser.length === 0){
+            return res.status(404).json({msg:"este usuario no existe"});
+        }
+    }
+    catch(error){
+        res.status(500).json({msg:"error al actualizar el perfil"});
+        console.error(error);
+    }
+};
+

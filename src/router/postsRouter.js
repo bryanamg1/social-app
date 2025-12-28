70c827d2f6d6977c1d0b04f2bf761a2705f5de9c
bryan marquez <bryan_amg1@icloud.com~>
2025-12-28 13:26:26 -0300

    fix post router

--- src/router/postsRouter.js
+++ src/router/postsRouter.js
@@ -1,10 +1,10 @@
 import Router from "express";
 import { addpost, allpost, postById, deletePostById } from "../controllers/postsController.js";
-import upload from "../middleware/upload.js";
+import { optionalUpload } from "../utils/utils.js";
 
 const router = Router();
 
-router.post("/CreatePost/:id", upload.single("image") , addpost);
+router.post("/CreatePost/:id", optionalUpload , addpost);
 router.get("/allpost", allpost)
 router.get("/postById/:id",postById )
 router.delete("/removePost/:id",deletePostById)

443d711bab51c5b0597ccca5f5e9af88f9c34258
bryan marquez <bryan_amg1@icloud.com~>
2025-12-29 19:47:47 -0300

    fix router with auth

--- src/router/imageRouter.js
+++ src/router/imageRouter.js
@@ -1,9 +1,10 @@
 import Router from "express";
 import {setImage} from "../controllers/userController.js"
 import { optionalUpload } from "../utils/utils.js";
+import auth from "../middleware/auth.js";
 
 const router = Router();
 
-router.post('/uploadImage/:userId', optionalUpload,setImage)
+router.post('/uploadImage/:userId', auth ,optionalUpload,setImage)
 
 export default router;

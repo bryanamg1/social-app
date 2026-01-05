e9b1a9fed536ce405486ae545fc7194f14b0896f
Denilson valero <denilsonvaleromail.com>
2026-01-05 15:20:55 -0300

    fix/message/error :bug:

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,7 +54,7 @@ export const login= async (req,res)=>{
         const [rows] = await db.query("SELECT * FROM users WHERE email = ?",[email]);
         
         if(rows.length === 0){
-            return res.status(400).json({msg:"email o contraseña incorrecta"});
+            return res.status(400).json({msg:"email no encontrado"});
         }
         const users = rows[0]
 

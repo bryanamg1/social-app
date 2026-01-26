f6002ac1c93c436d5f39255b58b02eff3415e35d
Denilson valero <denilsonvaleromail.com>
2026-01-26 16:30:55 -0300

    refactor/db

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,25 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+if (!process.env.DB_HOST) {
+  throw new Error("❌ Variables MYSQL no definidas (Railway)");
+}
+
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    port: process.env.DB_PORT || "",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "social_app"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT ),
+
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
-console.log('Conectado a la base de datos✔️​​');
+console.log("✅ Pool MySQL conectado correctamente");
 
 export default db;
-
-

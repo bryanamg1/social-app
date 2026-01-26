b67da5c8a6985b7236c921c73c92445ae4d56e9b
Denilson valero <denilsonvaleromail.com>
2026-01-26 15:59:09 -0300

    general/commit

--- src/server.js
+++ src/server.js
@@ -15,13 +15,13 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   }
 });
-setIO(io);
+/* setIO(io);
 // Registrar sockets
 notificationSocket(io);
 registerMessagesSocket(io);
-
+ */
 // Levantar servidor (Express + Socket en el MISMO puerto)
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Servidor iniciado en: http://localhost:${PORT}`);
   console.log(`✅ Socket namespace: /messages`);
 });
